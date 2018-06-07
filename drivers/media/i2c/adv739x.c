584f8284ced08b3b3c54e70c158dd64d9a9bcf07
Hartmut Keller <keller@fs-net.de>
2018-06-07 17:30:19 +0200

    Add support for ADV7391 video encoder
    The ADV7391 is similar to the ADV7393, it just misses 16 bit input. So
    change the existing ADV7393 driver to ADV739X and add code for ADV7391.

--- drivers/media/i2c/adv739x.c
+++ drivers/media/i2c/adv739x.c
@@ -31,20 +31,20 @@
 #include <linux/videodev2.h>
 #include <linux/uaccess.h>
 
-#include <media/adv7393.h>
+#include <media/adv739x.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ctrls.h>
 
-#include "adv7393_regs.h"
+#include "adv739x_regs.h"
 
-MODULE_DESCRIPTION("ADV7393 video encoder driver");
+MODULE_DESCRIPTION("ADV739X video encoder driver");
 MODULE_LICENSE("GPL");
 
 static bool debug;
 module_param(debug, bool, 0644);
 MODULE_PARM_DESC(debug, "Debug level 0-1");
 
-struct adv7393_state {
+struct adv739x_state {
 	struct v4l2_subdev sd;
 	struct v4l2_ctrl_handler hdl;
 	u8 reg00;
@@ -57,49 +57,49 @@ struct adv7393_state {
 	v4l2_std_id std;
 };
 
-static inline struct adv7393_state *to_state(struct v4l2_subdev *sd)
+static inline struct adv739x_state *to_state(struct v4l2_subdev *sd)
 {
-	return container_of(sd, struct adv7393_state, sd);
+	return container_of(sd, struct adv739x_state, sd);
 }
 
 static inline struct v4l2_subdev *to_sd(struct v4l2_ctrl *ctrl)
 {
-	return &container_of(ctrl->handler, struct adv7393_state, hdl)->sd;
+	return &container_of(ctrl->handler, struct adv739x_state, hdl)->sd;
 }
 
-static inline int adv7393_write(struct v4l2_subdev *sd, u8 reg, u8 value)
+static inline int adv739x_write(struct v4l2_subdev *sd, u8 reg, u8 value)
 {
 	struct i2c_client *client = v4l2_get_subdevdata(sd);
 
 	return i2c_smbus_write_byte_data(client, reg, value);
 }
 
-static const u8 adv7393_init_reg_val[] = {
-	ADV7393_SOFT_RESET, ADV7393_SOFT_RESET_DEFAULT,
-	ADV7393_POWER_MODE_REG, ADV7393_POWER_MODE_REG_DEFAULT,
+static const u8 adv739x_init_reg_val[] = {
+	ADV739X_SOFT_RESET, ADV739X_SOFT_RESET_DEFAULT,
+	ADV739X_POWER_MODE_REG, ADV739X_POWER_MODE_REG_DEFAULT,
 
-	ADV7393_HD_MODE_REG1, ADV7393_HD_MODE_REG1_DEFAULT,
-	ADV7393_HD_MODE_REG2, ADV7393_HD_MODE_REG2_DEFAULT,
-	ADV7393_HD_MODE_REG3, ADV7393_HD_MODE_REG3_DEFAULT,
-	ADV7393_HD_MODE_REG4, ADV7393_HD_MODE_REG4_DEFAULT,
-	ADV7393_HD_MODE_REG5, ADV7393_HD_MODE_REG5_DEFAULT,
-	ADV7393_HD_MODE_REG6, ADV7393_HD_MODE_REG6_DEFAULT,
-	ADV7393_HD_MODE_REG7, ADV7393_HD_MODE_REG7_DEFAULT,
+	ADV739X_HD_MODE_REG1, ADV739X_HD_MODE_REG1_DEFAULT,
+	ADV739X_HD_MODE_REG2, ADV739X_HD_MODE_REG2_DEFAULT,
+	ADV739X_HD_MODE_REG3, ADV739X_HD_MODE_REG3_DEFAULT,
+	ADV739X_HD_MODE_REG4, ADV739X_HD_MODE_REG4_DEFAULT,
+	ADV739X_HD_MODE_REG5, ADV739X_HD_MODE_REG5_DEFAULT,
+	ADV739X_HD_MODE_REG6, ADV739X_HD_MODE_REG6_DEFAULT,
+	ADV739X_HD_MODE_REG7, ADV739X_HD_MODE_REG7_DEFAULT,
 
-	ADV7393_SD_MODE_REG1, ADV7393_SD_MODE_REG1_DEFAULT,
-	ADV7393_SD_MODE_REG2, ADV7393_SD_MODE_REG2_DEFAULT,
-	ADV7393_SD_MODE_REG3, ADV7393_SD_MODE_REG3_DEFAULT,
-	ADV7393_SD_MODE_REG4, ADV7393_SD_MODE_REG4_DEFAULT,
-	ADV7393_SD_MODE_REG5, ADV7393_SD_MODE_REG5_DEFAULT,
-	ADV7393_SD_MODE_REG6, ADV7393_SD_MODE_REG6_DEFAULT,
-	ADV7393_SD_MODE_REG7, ADV7393_SD_MODE_REG7_DEFAULT,
-	ADV7393_SD_MODE_REG8, ADV7393_SD_MODE_REG8_DEFAULT,
+	ADV739X_SD_MODE_REG1, ADV739X_SD_MODE_REG1_DEFAULT,
+	ADV739X_SD_MODE_REG2, ADV739X_SD_MODE_REG2_DEFAULT,
+	ADV739X_SD_MODE_REG3, ADV739X_SD_MODE_REG3_DEFAULT,
+	ADV739X_SD_MODE_REG4, ADV739X_SD_MODE_REG4_DEFAULT,
+	ADV739X_SD_MODE_REG5, ADV739X_SD_MODE_REG5_DEFAULT,
+	ADV739X_SD_MODE_REG6, ADV739X_SD_MODE_REG6_DEFAULT,
+	ADV739X_SD_MODE_REG7, ADV739X_SD_MODE_REG7_DEFAULT,
+	ADV739X_SD_MODE_REG8, ADV739X_SD_MODE_REG8_DEFAULT,
 
-	ADV7393_SD_TIMING_REG0, ADV7393_SD_TIMING_REG0_DEFAULT,
+	ADV739X_SD_TIMING_REG0, ADV739X_SD_TIMING_REG0_DEFAULT,
 
-	ADV7393_SD_HUE_ADJUST, ADV7393_SD_HUE_ADJUST_DEFAULT,
-	ADV7393_SD_CGMS_WSS0, ADV7393_SD_CGMS_WSS0_DEFAULT,
-	ADV7393_SD_BRIGHTNESS_WSS, ADV7393_SD_BRIGHTNESS_WSS_DEFAULT,
+	ADV739X_SD_HUE_ADJUST, ADV739X_SD_HUE_ADJUST_DEFAULT,
+	ADV739X_SD_CGMS_WSS0, ADV739X_SD_CGMS_WSS0_DEFAULT,
+	ADV739X_SD_BRIGHTNESS_WSS, ADV739X_SD_BRIGHTNESS_WSS_DEFAULT,
 };
 
 /*
@@ -107,7 +107,7 @@ static const u8 adv7393_init_reg_val[] = {
  * FSC(reg) =  FSC (HZ) * --------
  *			  27000000
  */
-static const struct adv7393_std_info stdinfo[] = {
+static const struct adv739x_std_info stdinfo[] = {
 	{
 		/* FSC(Hz) = 4,433,618.75 Hz */
 		SD_STD_NTSC, 705268427, V4L2_STD_NTSC_443,
@@ -132,10 +132,10 @@ static const struct adv7393_std_info stdinfo[] = {
 	},
 };
 
-static int adv7393_setstd(struct v4l2_subdev *sd, v4l2_std_id std)
+static int adv739x_setstd(struct v4l2_subdev *sd, v4l2_std_id std)
 {
-	struct adv7393_state *state = to_state(sd);
-	const struct adv7393_std_info *std_info;
+	struct adv739x_state *state = to_state(sd);
+	const struct adv739x_std_info *std_info;
 	int num_std;
 	u8 reg;
 	u32 val;
@@ -161,7 +161,7 @@ static int adv7393_setstd(struct v4l2_subdev *sd, v4l2_std_id std)
 	/* Set the standard */
 	val = state->reg80 & ~SD_STD_MASK;
 	val |= std_info->standard_val3;
-	err = adv7393_write(sd, ADV7393_SD_MODE_REG1, val);
+	err = adv739x_write(sd, ADV739X_SD_MODE_REG1, val);
 	if (err < 0)
 		goto setstd_exit;
 
@@ -170,7 +170,7 @@ static int adv7393_setstd(struct v4l2_subdev *sd, v4l2_std_id std)
 	/* Configure the input mode register */
 	val = state->reg01 & ~INPUT_MODE_MASK;
 	val |= SD_INPUT_MODE;
-	err = adv7393_write(sd, ADV7393_MODE_SELECT_REG, val);
+	err = adv739x_write(sd, ADV739X_MODE_SELECT_REG, val);
 	if (err < 0)
 		goto setstd_exit;
 
@@ -178,8 +178,8 @@ static int adv7393_setstd(struct v4l2_subdev *sd, v4l2_std_id std)
 
 	/* Program the sub carrier frequency registers */
 	val = std_info->fsc_val;
-	for (reg = ADV7393_FSC_REG0; reg <= ADV7393_FSC_REG3; reg++) {
-		err = adv7393_write(sd, reg, val);
+	for (reg = ADV739X_FSC_REG0; reg <= ADV739X_FSC_REG3; reg++) {
+		err = adv739x_write(sd, reg, val);
 		if (err < 0)
 			goto setstd_exit;
 		val >>= 8;
@@ -193,7 +193,7 @@ static int adv7393_setstd(struct v4l2_subdev *sd, v4l2_std_id std)
 	else
 		val &= SD_PEDESTAL_DI;
 
-	err = adv7393_write(sd, ADV7393_SD_MODE_REG2, val);
+	err = adv739x_write(sd, ADV739X_SD_MODE_REG2, val);
 	if (err < 0)
 		goto setstd_exit;
 
@@ -206,13 +206,13 @@ setstd_exit:
 	return err;
 }
 
-static int adv7393_setoutput(struct v4l2_subdev *sd, u32 output_type)
+static int adv739x_setoutput(struct v4l2_subdev *sd, u32 output_type)
 {
-	struct adv7393_state *state = to_state(sd);
+	struct adv739x_state *state = to_state(sd);
 	u8 val;
 	int err = 0;
 
-	if (output_type > ADV7393_SVIDEO_ID) {
+	if (output_type > ADV739X_SVIDEO_ID) {
 		v4l2_dbg(1, debug, sd,
 			"Invalid output type or output type not supported:%d\n",
 								output_type);
@@ -222,14 +222,14 @@ static int adv7393_setoutput(struct v4l2_subdev *sd, u32 output_type)
 	/* Enable Appropriate DAC */
 	val = state->reg00 & 0x03;
 
-	if (output_type == ADV7393_COMPOSITE_ID)
-		val |= ADV7393_COMPOSITE_POWER_VALUE;
-	else if (output_type == ADV7393_COMPONENT_ID)
-		val |= ADV7393_COMPONENT_POWER_VALUE;
+	if (output_type == ADV739X_COMPOSITE_ID)
+		val |= ADV739X_COMPOSITE_POWER_VALUE;
+	else if (output_type == ADV739X_COMPONENT_ID)
+		val |= ADV739X_COMPONENT_POWER_VALUE;
 	else
-		val |= ADV7393_SVIDEO_POWER_VALUE;
+		val |= ADV739X_SVIDEO_POWER_VALUE;
 
-	err = adv7393_write(sd, ADV7393_POWER_MODE_REG, val);
+	err = adv739x_write(sd, ADV739X_POWER_MODE_REG, val);
 	if (err < 0)
 		goto setoutput_exit;
 
@@ -237,7 +237,7 @@ static int adv7393_setoutput(struct v4l2_subdev *sd, u32 output_type)
 
 	/* Enable YUV output */
 	val = state->reg02 | YUV_OUTPUT_SELECT;
-	err = adv7393_write(sd, ADV7393_MODE_REG0, val);
+	err = adv739x_write(sd, ADV739X_MODE_REG0, val);
 	if (err < 0)
 		goto setoutput_exit;
 
@@ -245,11 +245,11 @@ static int adv7393_setoutput(struct v4l2_subdev *sd, u32 output_type)
 
 	/* configure SD DAC Output 1 bit */
 	val = state->reg82;
-	if (output_type == ADV7393_COMPONENT_ID)
+	if (output_type == ADV739X_COMPONENT_ID)
 		val &= SD_DAC_OUT1_DI;
 	else
 		val |= SD_DAC_OUT1_EN;
-	err = adv7393_write(sd, ADV7393_SD_MODE_REG2, val);
+	err = adv739x_write(sd, ADV739X_SD_MODE_REG2, val);
 	if (err < 0)
 		goto setoutput_exit;
 
@@ -257,7 +257,7 @@ static int adv7393_setoutput(struct v4l2_subdev *sd, u32 output_type)
 
 	/* configure ED/HD Color DAC Swap bit to zero */
 	val = state->reg35 & HD_DAC_SWAP_DI;
-	err = adv7393_write(sd, ADV7393_HD_MODE_REG6, val);
+	err = adv739x_write(sd, ADV739X_HD_MODE_REG6, val);
 	if (err < 0)
 		goto setoutput_exit;
 
@@ -270,9 +270,9 @@ setoutput_exit:
 	return err;
 }
 
-static int adv7393_log_status(struct v4l2_subdev *sd)
+static int adv739x_log_status(struct v4l2_subdev *sd)
 {
-	struct adv7393_state *state = to_state(sd);
+	struct adv739x_state *state = to_state(sd);
 
 	v4l2_info(sd, "Standard: %llx\n", (unsigned long long)state->std);
 	v4l2_info(sd, "Output: %s\n", (state->output == 0) ? "Composite" :
@@ -280,32 +280,32 @@ static int adv7393_log_status(struct v4l2_subdev *sd)
 	return 0;
 }
 
-static int adv7393_s_ctrl(struct v4l2_ctrl *ctrl)
+static int adv739x_s_ctrl(struct v4l2_ctrl *ctrl)
 {
 	struct v4l2_subdev *sd = to_sd(ctrl);
 
 	switch (ctrl->id) {
 	case V4L2_CID_BRIGHTNESS:
-		return adv7393_write(sd, ADV7393_SD_BRIGHTNESS_WSS,
+		return adv739x_write(sd, ADV739X_SD_BRIGHTNESS_WSS,
 					ctrl->val & SD_BRIGHTNESS_VALUE_MASK);
 
 	case V4L2_CID_HUE:
-		return adv7393_write(sd, ADV7393_SD_HUE_ADJUST,
-					ctrl->val - ADV7393_HUE_MIN);
+		return adv739x_write(sd, ADV739X_SD_HUE_ADJUST,
+					ctrl->val - ADV739X_HUE_MIN);
 
 	case V4L2_CID_GAIN:
-		return adv7393_write(sd, ADV7393_DAC123_OUTPUT_LEVEL,
+		return adv739x_write(sd, ADV739X_DAC123_OUTPUT_LEVEL,
 					ctrl->val);
 	}
 	return -EINVAL;
 }
 
-static const struct v4l2_ctrl_ops adv7393_ctrl_ops = {
-	.s_ctrl = adv7393_s_ctrl,
+static const struct v4l2_ctrl_ops adv739x_ctrl_ops = {
+	.s_ctrl = adv739x_s_ctrl,
 };
 
-static const struct v4l2_subdev_core_ops adv7393_core_ops = {
-	.log_status = adv7393_log_status,
+static const struct v4l2_subdev_core_ops adv739x_core_ops = {
+	.log_status = adv739x_log_status,
 	.g_ext_ctrls = v4l2_subdev_g_ext_ctrls,
 	.try_ext_ctrls = v4l2_subdev_try_ext_ctrls,
 	.s_ext_ctrls = v4l2_subdev_s_ext_ctrls,
@@ -315,57 +315,57 @@ static const struct v4l2_subdev_core_ops adv7393_core_ops = {
 	.querymenu = v4l2_subdev_querymenu,
 };
 
-static int adv7393_s_std_output(struct v4l2_subdev *sd, v4l2_std_id std)
+static int adv739x_s_std_output(struct v4l2_subdev *sd, v4l2_std_id std)
 {
-	struct adv7393_state *state = to_state(sd);
+	struct adv739x_state *state = to_state(sd);
 	int err = 0;
 
 	if (state->std == std)
 		return 0;
 
-	err = adv7393_setstd(sd, std);
+	err = adv739x_setstd(sd, std);
 	if (!err)
 		state->std = std;
 
 	return err;
 }
 
-static int adv7393_s_routing(struct v4l2_subdev *sd,
+static int adv739x_s_routing(struct v4l2_subdev *sd,
 		u32 input, u32 output, u32 config)
 {
-	struct adv7393_state *state = to_state(sd);
+	struct adv739x_state *state = to_state(sd);
 	int err = 0;
 
 	if (state->output == output)
 		return 0;
 
-	err = adv7393_setoutput(sd, output);
+	err = adv739x_setoutput(sd, output);
 	if (!err)
 		state->output = output;
 
 	return err;
 }
 
-static const struct v4l2_subdev_video_ops adv7393_video_ops = {
-	.s_std_output	= adv7393_s_std_output,
-	.s_routing	= adv7393_s_routing,
+static const struct v4l2_subdev_video_ops adv739x_video_ops = {
+	.s_std_output	= adv739x_s_std_output,
+	.s_routing	= adv739x_s_routing,
 };
 
-static const struct v4l2_subdev_ops adv7393_ops = {
-	.core	= &adv7393_core_ops,
-	.video	= &adv7393_video_ops,
+static const struct v4l2_subdev_ops adv739x_ops = {
+	.core	= &adv739x_core_ops,
+	.video	= &adv739x_video_ops,
 };
 
-static int adv7393_initialize(struct v4l2_subdev *sd)
+static int adv739x_initialize(struct v4l2_subdev *sd)
 {
-	struct adv7393_state *state = to_state(sd);
+	struct adv739x_state *state = to_state(sd);
 	int err = 0;
 	int i;
 
-	for (i = 0; i < ARRAY_SIZE(adv7393_init_reg_val); i += 2) {
+	for (i = 0; i < ARRAY_SIZE(adv739x_init_reg_val); i += 2) {
 
-		err = adv7393_write(sd, adv7393_init_reg_val[i],
-					adv7393_init_reg_val[i+1]);
+		err = adv739x_write(sd, adv739x_init_reg_val[i],
+					adv739x_init_reg_val[i+1]);
 		if (err) {
 			v4l2_err(sd, "Error initializing\n");
 			return err;
@@ -373,13 +373,13 @@ static int adv7393_initialize(struct v4l2_subdev *sd)
 	}
 
 	/* Configure for default video standard */
-	err = adv7393_setoutput(sd, state->output);
+	err = adv739x_setoutput(sd, state->output);
 	if (err < 0) {
 		v4l2_err(sd, "Error setting output during init\n");
 		return -EINVAL;
 	}
 
-	err = adv7393_setstd(sd, state->std);
+	err = adv739x_setstd(sd, state->std);
 	if (err < 0) {
 		v4l2_err(sd, "Error setting std during init\n");
 		return -EINVAL;
@@ -388,10 +388,10 @@ static int adv7393_initialize(struct v4l2_subdev *sd)
 	return err;
 }
 
-static int adv7393_probe(struct i2c_client *client,
+static int adv739x_probe(struct i2c_client *client,
 				const struct i2c_device_id *id)
 {
-	struct adv7393_state *state;
+	struct adv739x_state *state;
 	int err;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE_DATA))
@@ -404,31 +404,31 @@ static int adv7393_probe(struct i2c_client *client,
 	if (state == NULL)
 		return -ENOMEM;
 
-	state->reg00	= ADV7393_POWER_MODE_REG_DEFAULT;
+	state->reg00	= ADV739X_POWER_MODE_REG_DEFAULT;
 	state->reg01	= 0x00;
 	state->reg02	= 0x20;
-	state->reg35	= ADV7393_HD_MODE_REG6_DEFAULT;
-	state->reg80	= ADV7393_SD_MODE_REG1_DEFAULT;
-	state->reg82	= ADV7393_SD_MODE_REG2_DEFAULT;
+	state->reg35	= ADV739X_HD_MODE_REG6_DEFAULT;
+	state->reg80	= ADV739X_SD_MODE_REG1_DEFAULT;
+	state->reg82	= ADV739X_SD_MODE_REG2_DEFAULT;
 
-	state->output = ADV7393_COMPOSITE_ID;
-	state->std = V4L2_STD_NTSC;
+	state->output = ADV739X_COMPOSITE_ID;
+	state->std = V4L2_STD_PAL;
 
-	v4l2_i2c_subdev_init(&state->sd, client, &adv7393_ops);
+	v4l2_i2c_subdev_init(&state->sd, client, &adv739x_ops);
 
 	v4l2_ctrl_handler_init(&state->hdl, 3);
-	v4l2_ctrl_new_std(&state->hdl, &adv7393_ctrl_ops,
-			V4L2_CID_BRIGHTNESS, ADV7393_BRIGHTNESS_MIN,
-					     ADV7393_BRIGHTNESS_MAX, 1,
-					     ADV7393_BRIGHTNESS_DEF);
-	v4l2_ctrl_new_std(&state->hdl, &adv7393_ctrl_ops,
-			V4L2_CID_HUE, ADV7393_HUE_MIN,
-				      ADV7393_HUE_MAX, 1,
-				      ADV7393_HUE_DEF);
-	v4l2_ctrl_new_std(&state->hdl, &adv7393_ctrl_ops,
-			V4L2_CID_GAIN, ADV7393_GAIN_MIN,
-				       ADV7393_GAIN_MAX, 1,
-				       ADV7393_GAIN_DEF);
+	v4l2_ctrl_new_std(&state->hdl, &adv739x_ctrl_ops,
+			V4L2_CID_BRIGHTNESS, ADV739X_BRIGHTNESS_MIN,
+					     ADV739X_BRIGHTNESS_MAX, 1,
+					     ADV739X_BRIGHTNESS_DEF);
+	v4l2_ctrl_new_std(&state->hdl, &adv739x_ctrl_ops,
+			V4L2_CID_HUE, ADV739X_HUE_MIN,
+				      ADV739X_HUE_MAX, 1,
+				      ADV739X_HUE_DEF);
+	v4l2_ctrl_new_std(&state->hdl, &adv739x_ctrl_ops,
+			V4L2_CID_GAIN, ADV739X_GAIN_MIN,
+				       ADV739X_GAIN_MAX, 1,
+				       ADV739X_GAIN_DEF);
 	state->sd.ctrl_handler = &state->hdl;
 	if (state->hdl.error) {
 		int err = state->hdl.error;
@@ -438,16 +438,16 @@ static int adv7393_probe(struct i2c_client *client,
 	}
 	v4l2_ctrl_handler_setup(&state->hdl);
 
-	err = adv7393_initialize(&state->sd);
+	err = adv739x_initialize(&state->sd);
 	if (err)
 		v4l2_ctrl_handler_free(&state->hdl);
 	return err;
 }
 
-static int adv7393_remove(struct i2c_client *client)
+static int adv739x_remove(struct i2c_client *client)
 {
 	struct v4l2_subdev *sd = i2c_get_clientdata(client);
-	struct adv7393_state *state = to_state(sd);
+	struct adv739x_state *state = to_state(sd);
 
 	v4l2_device_unregister_subdev(sd);
 	v4l2_ctrl_handler_free(&state->hdl);
@@ -455,19 +455,19 @@ static int adv7393_remove(struct i2c_client *client)
 	return 0;
 }
 
-static const struct i2c_device_id adv7393_id[] = {
-	{"adv7393", 0},
+static const struct i2c_device_id adv739x_id[] = {
+	{"adv739x", 0},
 	{},
 };
-MODULE_DEVICE_TABLE(i2c, adv7393_id);
+MODULE_DEVICE_TABLE(i2c, adv739x_id);
 
-static struct i2c_driver adv7393_driver = {
+static struct i2c_driver adv739x_driver = {
 	.driver = {
 		.owner	= THIS_MODULE,
-		.name	= "adv7393",
+		.name	= "adv739x",
 	},
-	.probe		= adv7393_probe,
-	.remove		= adv7393_remove,
-	.id_table	= adv7393_id,
+	.probe		= adv739x_probe,
+	.remove		= adv739x_remove,
+	.id_table	= adv739x_id,
 };
-module_i2c_driver(adv7393_driver);
+module_i2c_driver(adv739x_driver);
