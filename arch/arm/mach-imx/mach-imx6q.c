ffd0bd3b742ae7fb2a373857c09e319324eb97b7
Patrick Jakob <jakob@fs-net.de>
2022-02-01 08:58:54 +0100

    Add phy fixup for efusA9r2
    The rtl8211f_phy_fixup set the register 0x10 (LED_CNFG) to Mode B. In
    Mode B, LEDs are configured as follows:
    LED0 = not used
    LED1 = BLINK for Activity (10/100/1000)
    LED2 = ON for good Link (10/100/1000)

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -42,6 +42,19 @@
 #include "hardware.h"
 
 #if defined(CONFIG_FEC) || defined(CONFIG_FEC_MODULE)
+#define RTL8211F_PAGE_SELECT	0x1f
+
+static int rtl8211f_phy_fixup(struct phy_device *dev)
+{
+	/* Set LED2 for Link, LED1 for Activity */
+	phy_write(dev, RTL8211F_PAGE_SELECT, 0xd04);
+	phy_write(dev, 0x10, 0xae00);
+	/* restore to default page 0 */
+	phy_write(dev, RTL8211F_PAGE_SELECT, 0x0);
+
+	return 0;
+}
+
 static int dp83848_phy_fixup(struct phy_device *dev)
 {
 	u16 val;
@@ -163,6 +176,7 @@ static int ar8035_phy_fixup(struct phy_device *dev)
 
 #define PHY_ID_DP83848 	0x20005c90
 #define PHY_ID_AR8035 0x004dd072
+#define PHY_ID_RTL8211F 0x001cc916
 
 static void __init imx6q_enet_phy_init(void)
 {
@@ -177,6 +191,8 @@ static void __init imx6q_enet_phy_init(void)
 				ar8031_phy_fixup);
 		phy_register_fixup_for_uid(PHY_ID_AR8035, 0xffffffef,
 				ar8035_phy_fixup);
+		phy_register_fixup_for_uid(PHY_ID_RTL8211F, 0x001fffff,
+				rtl8211f_phy_fixup);
 	}
 }
 
