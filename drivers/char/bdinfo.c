a7ff346e5e2cbd3f0bba5b04b772ab328ff10556
Philipp Gerbach <gerbach@fs-net.de>
2023-02-09 16:24:06 +0100

    Improve bdinfo driver
    We want to be able to read the board-cfg and board-id from linux.
    
    For this the bdinfo driver  needs to be able to also parse subnodes for
    the bdinfo device tree node.
    
    The Uboot needs to add the board-cfg to the bdinfo device tree node as a
    subnode.
    Then the bdinfo driver can parse the subnode and add it as a
    subdirectory to the sysfs add /sys/bdinfo.
    
    This will currently work only with one subnode layer, so if we add an
    additional subnodes to the board-cfg, we will have to adapt the bdinfo
    driver again.
    
    A simple heuristic will try to determine the data type of each device
    tree entry. This may fail for certain circumstances, but should be ok
    most of the time.
    The heuristic is taken from
    scripts/dtc/fdtget.c
    and
    scripts/dtc/fdtdump.c

--- drivers/char/bdinfo.c
+++ drivers/char/bdinfo.c
@@ -25,15 +25,21 @@
 #include <linux/slab.h>			/* kzalloc() */
 #include <linux/platform_device.h>	/* struct platform_device */
 #include <linux/of.h>			/* __setup() */
+#include <linux/ctype.h>
+
+#define GET_CELL(p)	(p += 4, *((const uint32_t *)(p-4)))
+#define fdt32_to_cpu(x) be32_to_cpu(x)
 
 struct bdinfo {
 	const char **prop_name;
 	const char **prop_val;
+	int  *prop_length;
 	unsigned int acount;
 	unsigned int mode;
 	struct kobj_attribute *kattr;
 	struct attribute_group attr_group;
 	struct kobject *bdinfo_kobj;
+	struct bdinfo *bdinfo_child;
 };
 
 struct bdinfo bdi;
@@ -49,68 +55,131 @@ static int __init login_tty_setup(char *arg)
 
 __setup("login_tty=", login_tty_setup);
 
+static bool util_is_printable_string(const void *data, int len)
+{
+	const char *s = data;
+	const char *ss, *se;
+
+	/* zero length is not */
+	if (len == 0)
+		return 0;
+
+	/* must terminate with zero */
+	if (s[len - 1] != '\0')
+		return 0;
+
+	se = s + len;
+
+	while (s < se) {
+		ss = s;
+		while (s < se && *s && isprint((unsigned char)*s))
+			s++;
+
+		/* not zero, or not done yet */
+		if (*s != '\0' || s == ss)
+			return 0;
+
+		s++;
+	}
+
+	return 1;
+}
+
 static ssize_t bdinfo_show(struct kobject *kobj,
 				      struct kobj_attribute *attr, char *buf)
 {
-	int i;
+	int i,j;
+	int num = 0;
+	struct bdinfo *bdiTmp = &bdi;
 
-	for (i = 0; i < bdi.acount; i++) {
-		if (strcmp(bdi.prop_name[i],attr->attr.name) == 0) {
+	while (kobj != bdiTmp->bdinfo_kobj)
+	{
+		bdiTmp = bdiTmp->bdinfo_child;
+		if (bdiTmp == NULL)
+			return sprintf(buf, "%s\n", "Error, subnode not found");
+	}
+
+	for (i = 0; i < bdiTmp->acount; i++) {
+		if (strcmp(bdiTmp->prop_name[i],attr->attr.name) == 0) {
 			break;
 		}
 	}
+	/* If value is a string */
+	if (util_is_printable_string(bdiTmp->prop_val[i], bdiTmp->prop_length[i])) {
+		return sprintf(buf, "%s\n", bdiTmp->prop_val[i]);
+	}
+	/* If value is a int32 */
+	else if ((bdiTmp->prop_length[i] % 4) == 0) {
+		for (j = 0; j < bdiTmp->prop_length[i]; j += 4)
+			num += sprintf(buf + num,"0x%08x%s", fdt32_to_cpu(GET_CELL(bdiTmp->prop_val[i])),
+			       j < (bdiTmp->prop_length[i] - 4) ? " " : "");
+	}
+	else {
+		for (j = 0; j < bdiTmp->prop_length[i]; j++)
+			num += sprintf(buf + num,"%02x%s", *bdiTmp->prop_val[i]++, j < bdiTmp->prop_length[i] - 1 ? " " : "");
+	}
 
-	return sprintf(buf, "%s\n", bdi.prop_val[i]);
+	return num;
 }
 
 static void bdinfo_free(void)
 {
-	if (bdi.attr_group.attrs) {
-		kfree(bdi.attr_group.attrs);
-		bdi.attr_group.attrs = NULL;
-	}
+	struct bdinfo *bdiTmp = &bdi;
+	struct bdinfo *bdiTmp_next = NULL;
 
-	if (bdi.kattr) {
-		kfree(bdi.kattr);
-		bdi.kattr = NULL;
+	while(bdiTmp != NULL)
+	{
+		if (bdiTmp->attr_group.attrs) {
+			kfree(bdiTmp->attr_group.attrs);
+			bdiTmp->attr_group.attrs = NULL;
+		}
+
+		if (bdiTmp->kattr) {
+			kfree(bdiTmp->kattr);
+			bdiTmp->kattr = NULL;
+		}
+
+		bdiTmp_next = bdiTmp->bdinfo_child;
+		bdiTmp->bdinfo_child = NULL;
+		bdiTmp = bdiTmp_next;
 	}
 }
 
-static int __init bdinfo_alloc(struct platform_device *pdev)
+static int bdinfo_alloc(struct platform_device *pdev, struct bdinfo *pbdi)
 {
 	struct kobj_attribute *p;
 	unsigned int i;
 
-	bdi.kattr = kzalloc(bdi.acount * sizeof(struct kobj_attribute),
+	pbdi->kattr = kzalloc(pbdi->acount * sizeof(struct kobj_attribute),
 								GFP_KERNEL);
-	if (!bdi.kattr) {
+	if (!pbdi->kattr) {
 		dev_err(&pdev->dev, "cannot allocate sysfs attributes!\n");
 		return -ENOMEM;
 	}
 
-	p = bdi.kattr;
+	p = pbdi->kattr;
 
-	for (i = 0; i < bdi.acount; i++) {
+	for (i = 0; i < pbdi->acount; i++) {
 		sysfs_attr_init(&(p->attr));
-		p->attr.name = bdi.prop_name[i];
-		p->attr.mode = bdi.mode;
+		p->attr.name = pbdi->prop_name[i];
+		p->attr.mode = pbdi->mode;
 		p->show = bdinfo_show;
 		p->store = NULL;
 		p++;
 	}
 
-	memset(&bdi.attr_group, 0, sizeof(struct attribute_group));
-	bdi.attr_group.attrs = kzalloc((bdi.acount+1) *
+	memset(&pbdi->attr_group, 0, sizeof(struct attribute_group));
+	pbdi->attr_group.attrs = kzalloc((pbdi->acount+1) *
 				sizeof(struct attribute *), GFP_KERNEL);
-	if (!bdi.attr_group.attrs) {
+	if (!pbdi->attr_group.attrs) {
 		dev_err(&pdev->dev, "cannot allocate attrib pointers!\n");
 		return -ENOMEM;
 	}
 
-	for (i = 0; i < bdi.acount; i++)
-		bdi.attr_group.attrs[i] = &bdi.kattr[i].attr;
+	for (i = 0; i < pbdi->acount; i++)
+		pbdi->attr_group.attrs[i] = &pbdi->kattr[i].attr;
 
-	bdi.attr_group.attrs[bdi.acount] = NULL;
+	pbdi->attr_group.attrs[pbdi->acount] = NULL;
 
 	return 0;
 }
@@ -118,9 +187,14 @@ static int __init bdinfo_alloc(struct platform_device *pdev)
 static int bdinfo_remove(struct platform_device *pdev)
 {
 	/* ###TODO: get from pdev */
-	if (bdi.bdinfo_kobj) {
-		kobject_put(bdi.bdinfo_kobj);
-		bdi.bdinfo_kobj = NULL;
+	struct bdinfo *bdiTmp = &bdi;
+	while(bdiTmp != NULL)
+	{
+		if (bdiTmp->bdinfo_kobj) {
+			kobject_put(bdiTmp->bdinfo_kobj);
+			bdiTmp->bdinfo_kobj = NULL;
+		}
+		bdiTmp = bdiTmp->bdinfo_child;
 	}
 
 	bdinfo_free();
@@ -145,9 +219,12 @@ static int login_tty_init(struct platform_device *pdev, int i)
 		memcpy(buf, login_tty, len);
 		buf[len++] = 0;
 		bdi.prop_val[i] = buf;
+		bdi.prop_length[i] = len;
 	} else
+	{
 		bdi.prop_val[i] = login_tty;
-
+		bdi.prop_length[i] = strlen(login_tty);
+	}
 	i++;
 
 	bdi.prop_name[i] = "login_speed";
@@ -156,17 +233,60 @@ static int login_tty_init(struct platform_device *pdev, int i)
 		to = "0";
 
 	bdi.prop_val[i] = to;
-
+	bdi.prop_length[i] = strlen(to) + 1;
 	return 0;
 }
 
-static int bdinfo_dt_init(struct platform_device *pdev)
+static int dt_init(struct platform_device *pdev, struct bdinfo *pbdi, const char *prefix,
+				   struct device_node *of_node)
 {
 	struct property *prop;
-	const char *prefix = "bdinfo";
 	size_t len = strlen(prefix);
 	int i = 0;
+
+	for_each_property_of_node(of_node, prop) {
+		if ((!strncmp(prop->name, "name", 4) &&
+					!strncmp(prop->value, prefix, len)) ||
+					!strncmp(prop->name, "compatible", 10))
+			continue;
+		pbdi->acount++;
+	}
+
+	pbdi->prop_name = devm_kzalloc(&pdev->dev, pbdi->acount *
+						sizeof(char *), GFP_KERNEL);
+
+	if (!pbdi->prop_name)
+		return -ENOMEM;
+
+	pbdi->prop_val = devm_kzalloc(&pdev->dev, pbdi->acount *
+						sizeof(char *), GFP_KERNEL);
+	if (!pbdi->prop_val)
+		return -ENOMEM;
+
+	pbdi->prop_length = devm_kzalloc(&pdev->dev, pbdi->acount *
+						sizeof(int), GFP_KERNEL);
+	if (!pbdi->prop_val)
+		return -ENOMEM;
+
+	for_each_property_of_node(of_node, prop) {
+		if ((!strncmp(prop->name, "name", 4) &&
+					!strncmp(prop->value, prefix, len)) ||
+					!strncmp(prop->name, "compatible", 10))
+			continue;
+
+		pbdi->prop_val[i] = prop->value;
+		pbdi->prop_name[i] = prop->name;
+		pbdi->prop_length[i] = prop->length;
+		i++;
+	}
+	return i;
+}
+
+static int bdinfo_init(struct platform_device *pdev)
+{
+	const char *prefix = "bdinfo";
 	int ret = 0;
+	int i = 0;
 
 	bdi.acount = 0;
 	bdi.mode = 0400;
@@ -175,57 +295,20 @@ static int bdinfo_dt_init(struct platform_device *pdev)
 	if (login_tty)
 		bdi.acount = 2;
 
-	for_each_property_of_node(pdev->dev.of_node, prop) {
-		if ((!strncmp(prop->name, "name", 4) &&
-					!strncmp(prop->value, prefix, len)) ||
-					!strncmp(prop->name, "compatible", 10))
-			continue;
-		bdi.acount++;
-	}
-
-	bdi.prop_name = devm_kzalloc(&pdev->dev, bdi.acount *
-						sizeof(char *), GFP_KERNEL);
-
-	if (!bdi.prop_name)
-		return -ENOMEM;
-
-	bdi.prop_val = devm_kzalloc(&pdev->dev, bdi.acount *
-						sizeof(char *), GFP_KERNEL);
-	if (!bdi.prop_val)
-		return -ENOMEM;
-
-	for_each_property_of_node(pdev->dev.of_node, prop) {
-		if ((!strncmp(prop->name, "name", 4) &&
-					!strncmp(prop->value, prefix, len)) ||
-					!strncmp(prop->name, "compatible", 10))
-			continue;
-
-		bdi.prop_val[i] = prop->value;
-		bdi.prop_name[i] = prop->name;
-		i++;
-	}
+	i = dt_init(pdev, &bdi, prefix, pdev->dev.of_node);
 
 	if (login_tty) {
 		ret = login_tty_init(pdev, i);
 	}
 
-	return ret;
-}
-
-static int bdinfo_probe(struct platform_device *pdev)
-{
-	int ret = 0;
-
-	ret = bdinfo_dt_init(pdev);
-
 	if (ret)
-		goto exit;
+		return ret;
 
-	ret = bdinfo_alloc(pdev);
+	ret = bdinfo_alloc(pdev, &bdi);
 
 	/* In case of error free everything that we allocated */
 	if (ret)
-		goto exit;
+		return ret;
 
 	bdi.bdinfo_kobj = kobject_create_and_add("bdinfo", NULL);
 
@@ -235,6 +318,56 @@ static int bdinfo_probe(struct platform_device *pdev)
 	} else
 		ret = sysfs_create_group(bdi.bdinfo_kobj, &bdi.attr_group);
 
+	bdi.bdinfo_child = NULL;
+
+	return ret;
+}
+
+static int bdinfo_subnode_init(struct platform_device *pdev)
+{
+	int ret = 0;
+	struct device_node *child;
+	struct bdinfo *bdinfo_tmp = &bdi;
+
+	for_each_child_of_node(pdev->dev.of_node, child)
+	{
+		bdinfo_tmp->bdinfo_child = devm_kzalloc(&pdev->dev,sizeof(struct bdinfo), GFP_KERNEL);
+
+		if(!bdinfo_tmp->bdinfo_child)
+			return -ENOMEM;
+
+		bdinfo_tmp = bdinfo_tmp->bdinfo_child;
+		bdinfo_tmp->acount = 0;
+		bdinfo_tmp->mode = 0400;
+
+		dt_init(pdev, bdinfo_tmp, child->name, child);
+		ret = bdinfo_alloc(pdev, bdinfo_tmp);
+
+		if (ret)
+			return ret;
+
+		bdinfo_tmp->bdinfo_kobj = kobject_create_and_add(child->name, bdi.bdinfo_kobj);
+
+		if (!bdi.bdinfo_kobj) {
+			dev_err(&pdev->dev, "cannot create kobject!\n");
+			ret = -ENOMEM;
+		} else
+			ret = sysfs_create_group(bdinfo_tmp->bdinfo_kobj, &bdinfo_tmp->attr_group);
+	}
+
+	bdinfo_tmp->bdinfo_child = NULL;
+	return ret;
+}
+
+static int bdinfo_probe(struct platform_device *pdev)
+{
+	int ret = 0;
+
+	ret = bdinfo_init(pdev);
+	if (ret)
+		goto exit;
+
+	bdinfo_subnode_init(pdev);
 	if (ret)
 		goto exit;
 
